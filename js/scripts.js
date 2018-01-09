b6afff2cad638a19bd1b6e090233d6697cd7489c
Frank Ngo and Kimberly Huynh <kimberly@>
2018-01-09 14:31:43 -0800

    add all click script to all definition

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,66 @@
 $(document).ready(function(){
-  $("#clickable"). click(function(){
-    $(".definition").toggle();
+  $(".clickable"). click(function(){
+    $(".definition-hidden").toggle();
+    $(".definition-shown").toggle();
   });
-
-
-
-
-
-
-
-
-
+  $(".clickable-2"). click(function(){
+    $(".definition-hidden-2").toggle();
+    $(".definition-shown-2").toggle();
+  });
+  $(".clickable-3"). click(function(){
+    $(".definition-hidden-3").toggle();
+    $(".definition-shown-3").toggle();
+  });
+  $(".clickable-4"). click(function(){
+    $(".definition-hidden-4").toggle();
+    $(".definition-shown-4").toggle();
+  });
+  $(".clickable-5"). click(function(){
+    $(".definition-hidden-5").toggle();
+    $(".definition-shown-5").toggle();
+  });
+  $(".clickable-6"). click(function(){
+    $(".definition-hidden-6").toggle();
+    $(".definition-shown-6").toggle();
+  });
+  $(".clickable-7"). click(function(){
+    $(".definition-hidden-7").toggle();
+    $(".definition-shown-7").toggle();
+  });
+  $(".clickable-8"). click(function(){
+    $(".definition-hidden-8").toggle();
+    $(".definition-shown-8").toggle();
+  });
+  $(".clickable-9"). click(function(){
+    $(".definition-hidden-9").toggle();
+    $(".definition-shown-9").toggle();
+  });
+  $(".clickable-10"). click(function(){
+    $(".definition-hidden-10").toggle();
+    $(".definition-shown-10").toggle();
+  });
+  $(".clickable-11"). click(function(){
+    $(".definition-hidden-11").toggle();
+    $(".definition-shown-11").toggle();
+  });
+  $(".clickable-12"). click(function(){
+    $(".definition-hidden-12").toggle();
+    $(".definition-shown-12").toggle();
+  });
+  $(".clickable-13"). click(function(){
+    $(".definition-hidden-13").toggle();
+    $(".definition-shown-13").toggle();
+  });
+  $(".clickable-14"). click(function(){
+    $(".definition-hidden-14").toggle();
+    $(".definition-shown-14").toggle();
+  });
+  $(".clickable-15"). click(function(){
+    $(".definition-hidden-15").toggle();
+    $(".definition-shown-15").toggle();
+  });
+  $(".clickable-16"). click(function(){
+    $(".definition-hidden-16").toggle();
+    $(".definition-shown-16").toggle();
+  });
 });
